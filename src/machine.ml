52888a7cd571eb445b096fd9e43eb955fe2a70bb
dbelpaum <dbelpaum@github.com>
2024-11-26 10:57:57 +0100

    fix: fixed end of frame

--- src/machine.ml
+++ src/machine.ml
@@ -22,12 +22,12 @@ let print_machine machine =
       Printf.printf "%s: [ %s ]\n" label (String.concat ", " lst)
     in
 
-    let print_frame label =
+    let print_frame label = (* Header frame *)
       let width = 80 in
       let border = String.make width '*' in
       let spacer = 
-        let padding = String.make (width - 2) ' ' in  (* Crée une ligne avec `width - 2` espaces *)
-        "*" ^ padding ^ "*"  (* Concatène le * de chaque côté de la chaîne d'espaces *)
+        let padding = String.make (width - 2) ' ' in
+        "*" ^ padding ^ "*"
       in
       let total_padding = width - String.length label - 2 in
       let left_padding = total_padding / 2 in
@@ -37,20 +37,18 @@ let print_machine machine =
       Printf.printf "%s\n%s\n*%s%s%s*\n%s\n%s\n" border spacer padding_left label padding_right spacer border    
     in
 
-    (* Affichage de l'encadré principal avec le nom de la machine *)
     print_frame machine.name;
-	
 		print_list "Alphabet" machine.alphabet;
 		print_list "States" machine.states;
 		Printf.printf "Initial: %s\n" machine.initial;
 		print_list "Finals" machine.finals;
 	
-		(* Affichage des transitions *)
 		List.iter (fun (state, transitions) ->
 			List.iter (fun t ->
 				Printf.printf "(%s, %s) -> (%s, %s, %s)\n"
 					state t.read t.to_state t.write t.action
 			) transitions
 		) machine.transitions;
-	
-		Printf.printf "*************************************\n";
+    
+    let end_frame = String.make 80 '*' in
+		  Printf.printf "%s\n" end_frame;
